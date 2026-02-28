f8aa788df255985f628a79eb7add12bf84d18765
poojapf <pooja@paperflite.com>
2026-02-28 16:29:03 +0530

    chrome-tab-url-checks

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import userReducer from "./userReducer";
+import selectionReducer from "./selectionReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     user: userReducer,
+    selection: selectionReducer,
 });
 
 export default rootReducer;
